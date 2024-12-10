f2a49b5f63f9c244fac02235fbf6243aa1635fcc
Lucas Oliveira <lucasvmo42@gmail.com>
2024-12-10 10:47:36 -0300

    Primeiro commit

---
+++ src/main/java/com/generation/deliveygeneration/DeliveyGenerationApplication.java
@@ -0,0 +1,13 @@
+package com.generation.deliveygeneration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveyGenerationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveyGenerationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/generation/deliveygeneration/DeliveyGenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.deliveygeneration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveyGenerationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
